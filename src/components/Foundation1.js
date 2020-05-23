101aa442508195b320583fba90ae11c8612c43ff
Rob W <robpwatkins@gmail.com>
2020-05-23 12:53:51 -0500

    poooool timeeee

--- src/components/Foundation1.js
+++ src/components/Foundation1.js
@@ -14,9 +14,38 @@ const Foundation1Div = styled.div`
 const Foundation1 = (props) => {
   const [cardStack, setCardStack] = useState([]);
   
-  let topCard = cardStack[cardStack.length - 1];
+  let topCard;
   
   const prevCard = usePreviousValue(topCard);
+  
+  // const handleClick = () => {
+  //   if (props.clickedCard.length > 0) {
+  //     let incomingCard = props.clickedCard[0];
+  //     if (cardStack.length === 0) {
+  //       if (incomingCard.rank !== 1) {
+  //         return;
+  //       }
+  //     }
+  //     let incomingCardStr = JSON.stringify(incomingCard);
+  //     let prevCardStr = JSON.stringify(prevCard);
+  //     if (incomingCardStr !== prevCardStr) {
+  //       return;
+  //     }
+  //     if (incomingCard.rank !== topCard.rank + 1) {
+  //       return;
+  //     }
+  //     setCardStack([cardStack, ...props.clickedCard]);
+  //     props.removeCard();
+  //     // setCurrentRank(incomingRank);
+  //   }
+  //   if (cardStack.length === 0) return; 
+  //   else {
+  //     let cards = cardStack;
+  //     cards.splice(cards.length - 1);
+  //     setCardStack(...cards);
+  //     // setCurrentRank(currentRank - 1);
+  //   }
+  // }
 
   const handleClick = () => {
     if (props.clickedCard.length > 0) {
@@ -24,24 +53,28 @@ const Foundation1 = (props) => {
       let incomingCardStr = JSON.stringify(incomingCard);
       let prevCardStr = JSON.stringify(prevCard);
       if (cardStack.length === 0) {
-        if (incomingCard.rank !== 1) {
-          return;
+        if (incomingCardStr !== prevCardStr || incomingCard.rank !== 1) return;
+      }
+      if (incomingCard.rank === topCard.rank + 1) {
+        let incomingCardColor;
+        let topCardColor;
+        if (incomingCard.suit === 'Spades!' || incomingCard.suit === 'Clubs!') {
+          incomingCardColor = 'B';
+        } else
+        incomingCardColor = 'R';
+        if (topCard.suit === 'Spades!' || topCard.suit === 'Clubs!') {
+          incomingCardColor = 'B';
+        } else
+        topCardColor = 'R';
+        if (incomingCardColor !== topCardColor) {
+          setCardStack([...cardStack, ...props.clickedCard]);
+          props.removeCard();
         }
       }
-      if (incomingCard.rank !== topCard.rank + 1) {
-        return;
-      }
-      setCardStack([cardStack, ...props.clickedCard]);
-      props.removeCard();
-      // setCurrentRank(incomingRank);
-    } else {
-      let cards = cardStack;
-      cards.splice(cards.length - 1);
-      setCardStack(...cards);
-      // setCurrentRank(currentRank - 1);
     }
-  }
-
+    setCardStack([...cardStack, ...props.clickedCard]);
+    props.removeCard();
+  }  
 
   return (
     <Foundation1Div onClick={handleClick}>
